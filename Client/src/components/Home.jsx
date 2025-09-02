e486c530cad4bd36c0706836e0a4111720007534
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-09-02 21:59:36 +0100

    Refactor HeroSection and Home components for improved readability and consistency; enhance layout and styling

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -1,42 +1,36 @@
-import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
-import HeroSection from './HeroSection'
-import CategoryCarousel from './CategoryCarousel'
-import LatestJobs from './LatestJobs'
-import Footer from './shared/Footer'
-import useGetAllJobs from '@/hooks/useGetAllJobs'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-
-
+import React, { useEffect } from 'react';
+import Navbar from './shared/Navbar';
+import HeroSection from './HeroSection';
+import CategoryCarousel from './CategoryCarousel';
+import LatestJobs from './LatestJobs';
+import Footer from './shared/Footer';
+import useGetAllJobs from '@/hooks/useGetAllJobs';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-
   const naviagte = useNavigate();
   useGetAllJobs();
-  const { user } = useSelector(store => store.auth)
+  const { user } = useSelector((store) => store.auth);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (user?.role === "recruiter") {
-      naviagte("/admin/companies");
+    if (user?.role === 'recruiter') {
+      naviagte('/admin/companies');
     }
-    
-
-
-  }, [user, naviagte])
-
+  }, [user, naviagte]);
 
   return (
-    <div>
+    <div className='min-h-screen flex flex-col'>
       <Navbar />
-      <HeroSection />
-      <CategoryCarousel />
-      <LatestJobs />
+      <div className='flex-1'>
+        <HeroSection />
+        <CategoryCarousel />
+        <LatestJobs />
+      </div>
       <Footer />
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

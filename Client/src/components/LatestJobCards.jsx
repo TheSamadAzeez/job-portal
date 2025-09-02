6cb06c2301a18ff5aebedda7b345bf97016343c7
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-09-02 21:59:16 +0100

    Refactor Job, JobDescription, Jobs, and LatestJobCards components for improved readability and consistency; enhance styling and functionality

--- Client/src/components/LatestJobCards.jsx
+++ Client/src/components/LatestJobCards.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Badge } from './ui/badge'
-import { Ghost } from 'lucide-react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Badge } from './ui/badge';
+import { Ghost } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 function LatestJobCards({ job }) {
-
   const navigate = useNavigate();
 
-
   return (
-    <div onClick={() => navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-lg bg-white border border-gray-100 cursor-pointer hover:scale-110 transition-all duration-600 '>
+    <div
+      onClick={() => navigate(`/description/${job._id}`)}
+      className='p-5 rounded-md shadow-lg bg-white border border-gray-100 cursor-pointer hover:scale-110 transition-all duration-600 '
+    >
       <div>
         <h1 className='font-medium text-lg'>{job?.company?.companyName}</h1>
         <p className='text-sm text-gray-500'>India</p>
@@ -19,23 +20,18 @@ function LatestJobCards({ job }) {
         <p className='text-sm text-gray-600'>{job?.description}</p>
       </div>
       <div className='flex items-center gap-2 mt-4'>
-        <Badge className="text-blue-700 font-bold" variant="ghost">
+        <Badge className='text-blue-700 font-bold' variant='ghost'>
           {job?.position} Positions
         </Badge>
-        <Badge className="text-[#F83002] font-bold" variant="ghost">
+        <Badge className='text-[#F83002] font-bold' variant='ghost'>
           {job?.jobType}
         </Badge>
-        <Badge className="text-[#7209b7] font-bold" variant="ghost">
-          {job?.salary} LPA
+        <Badge className='text-[#7209b7] font-bold' variant='ghost'>
+          ₦{job?.salary}
         </Badge>
-
       </div>
-
-
-
     </div>
-
-  )
+  );
 }
 
-export default LatestJobCards
+export default LatestJobCards;
